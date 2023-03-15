75f1787f80c30f842d5e4e78d1d688fefc4bbf01
Chinnawich <hotlnw159@gmail.com>
2023-03-15 15:55:03 +0700

    update index

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -32,12 +32,16 @@ const Index = () => {
       ) : (
         <div>
           {data && (
-            <div>
-              <h2 className="text-3xl font-bold">{data.name}</h2>
-              <p>{data.description}</p>
-              <p>{data.address.zip}</p>
-              <p>{data.address.street}</p>
-              <p>{data.address.province}</p>
+            <div className="bg-slate-700 p-6 rounded-md container w-96">
+              <h2 className="text-3xl font-bold text-white underline mb-3">
+                {data.name}
+              </h2>
+              <p className="text-white mb-1">{data.description}</p>
+              <p className="text-white mb-1">{data.address.street}</p>
+              <p className="text-white mb-1 font-bold">
+                <span className="mr-3">{data.address.zip}</span>
+                {data.address.province}
+              </p>
             </div>
           )}
         </div>
