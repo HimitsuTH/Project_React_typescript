59ff3c23d7f3e77aeff33b9ad86a2dbb6db500de
chinnawich <hotlnw159@gmail.com>
2023-03-31 14:47:46 +0700

    add function `edit` && `delete`

--- src/components/pages/headphone/Card.tsx
+++ src/components/pages/headphone/Card.tsx
@@ -1,8 +1,52 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+import IUser from "@/types/Auth";
 import { headphone } from "@/types/types";
 
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+import { delete_Headphone } from "@/services/brand.service";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import EditIcon from "@mui/icons-material/Edit";
+
 function Card({ ...headphone }: headphone) {
-  //   console.log(headphone);
+  const user = localStorage.getItem("user");
+  const currentUser: IUser = user ? JSON.parse(user) : {};
+  const handleDelete = (id: string) => {
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div className="custom-ui">
+            <h1 className="text-white">Are you sure?</h1>
+            <p className="text-white">You want to delete this headphone?</p>
+            <div className="grid grid-cols-2 gap-2 mt-2">
+              <button onClick={onClose}>No</button>
+              <button
+                onClick={() => {
+                  delete_Headphone(headphone.id)
+                    .then(
+                      (res) => {
+                        console.log(res);
+                        alert(res.data.message);
+                      },
+                      (err) => {
+                        alert(err.response.data.message);
+                      }
+                    )
+                    .then(() => window.location.reload());
+                }}
+              >
+                Yes, Delete it!
+              </button>
+            </div>
+          </div>
+        );
+      },
+    });
+  };
   return (
     <div className="p-10 w-80 bg-slate-300 drop-shadow-md rounded-lg relative select-none ">
       <p className=" font-bold text-center">{headphone.name}</p>
@@ -10,6 +54,21 @@ function Card({ ...headphone }: headphone) {
       <p className=" text-blue-700 absolute bottom-0 m-2 right-2">
         THB {headphone.price} Bath.
       </p>
+      {currentUser?.role == "admin" && (
+        <div className="flex absolute top-0 m-3 right-1">
+          <Link to={`/headphone/${headphone.id}/update`} className="flex">
+            <EditIcon
+              className="cursor-pointer text-slate-500 "
+              sx={{ "&:hover": { color: "#7877E6" } }}
+            />
+          </Link>
+          <DeleteIcon
+            onClick={() => handleDelete(headphone.id)}
+            className="cursor-pointer text-slate-500 "
+            sx={{ "&:hover": { color: "#7877E6" } }}
+          />
+        </div>
+      )}
     </div>
   );
 }
